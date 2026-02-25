f972680d7365d07107f41573e4f66bce1baa1fbe
quangvu0407 <trantuyenquang0407@gmail.com>
2026-02-25 18:22:46 +0700

    Thuc tap doanh nghiep

---
+++ admin/src/pages/List.jsx
@@ -0,0 +1,82 @@
+import axiosInstance from "../customize/axios"
+import React, { useEffect, useState } from "react";
+import {currency } from "../App";
+import { toast } from "react-toastify";
+
+const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const data = await axiosInstance.get("/api/product/list");
+
+      if (data.success) {
+        setList(data.products);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const removeProduct = async (id) => {
+    try {
+      const data = await axiosInstance.delete(`/api/product/${id}`);
+      if (data.success) {
+        toast.success(data.message);
+        await fetchList();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <>
+      <p className="mb-2">All Products List</p>
+      <div className="flex flex-col gap-2">
+        {/* -------- List Table Title -------- */}
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="text-center">Action</b>
+        </div>
+
+        {/* -------- Product List -------- */}
+        {list.map((item, index) => (
+          <div
+            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm"
+            key={index}
+          >
+            <img className="w-12" src={item.image[0]} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>
+              {currency}
+              {item.price}
+            </p>
+            <p
+              onClick={() => removeProduct(item._id)}
+              className="text-right md:text-center cursor-pointer text-lg"
+            >
+              X
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default List;
